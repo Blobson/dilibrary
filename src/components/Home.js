61bce885bd27f06a9b0302d9cfc2d0d991657c3d
Vladimir Klimov <blobson@gmail.com>
2024-11-03 20:11:23 +0300

    Исправление line-height (#4)

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React, { useContext } from "react"
-import { Link } from "react-router-dom"
 import styled from "styled-components"
 
 import { CatalogContext } from '../data/CatalogProvider'
--- src/components/Masters.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-
-const Masters = () => (
-  <Fragment>
-  </Fragment>
-)
-
-export default Masters
